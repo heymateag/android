bce3c1602db9a5d93ae88984609ef67661914b43
Yashar <yashar.p@780.ir>
2021-03-12 16:46:00 +0330

    Implementing texts resources structure

---
+++ TMessagesProj/src/main/java/works/heymate/core/Texts.java
@@ -0,0 +1,15 @@
+package works.heymate.core;
+
+import android.content.Context;
+
+public class Texts {
+
+    public static final String
+
+    private static Context mContext;
+
+    public static void initialize(Context context) {
+        mContext = context;
+    }
+
+}
